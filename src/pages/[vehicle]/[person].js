74648c9ffa7567a49d3dca204c23b126baf874b5
Mitch Lum <mklum88@gmail.com>
2020-06-15 16:11:05 -0700

    installed typescript and changed file names

--- src/pages/[vehicle]/[person].js
+++ src/pages/[vehicle]/[person].js
@@ -1,10 +1,45 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-export default function Person() {
+export default function Person({ ownersList }) {
   const router = useRouter();
-  return (
-    <h2>
-      {router.query.person}'s {router.query.vehicle}
-    </h2>
-  );
+  const [owners, setOwners] = useState(ownersList);
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch(
+        "http://localhost:4001/vehicles?ownerName=" +
+          router.query.person +
+          "&vehicle=" +
+          router.query.vehicle
+      );
+      const ownersList = await response.json();
+      setOwners(ownersList);
+    }
+
+    if (ownersList.length == 0) {
+      loadData();
+    }
+  }, []);
+
+  if (!owners[0]) {
+    return <div>Loading...</div>;
+  }
+
+  return <pre>{owners[0] ? owners[0].details : "No Details"}</pre>;
 }
+
+Person.getInitialProps = async (ctx) => {
+  if (!ctx.req) {
+    return { ownersList: [] };
+  }
+
+  const { query } = ctx;
+  const response = await fetch(
+    "http://localhost:4001/vehicles?ownerName=" +
+      query.person +
+      "&vehicle=" +
+      query.vehicle
+  );
+  const ownersList = await response.json();
+  return { ownersList: ownersList };
+};
